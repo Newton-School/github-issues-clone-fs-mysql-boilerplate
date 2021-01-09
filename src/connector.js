fe3beaecef6dd4a646451bb1633d1557c889dadd
EdOates84 <niteshmahawer1@gmail.com>
2021-01-09 10:58:35 +0530

    add createdatabase

--- src/connector.js
+++ src/connector.js
@@ -2,7 +2,7 @@
 var mysql = require('mysql');
 const { data } = require('./data')
 
-var con = mysql.createConnection({
+var connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "a",
@@ -11,35 +11,9 @@ var con = mysql.createConnection({
 });
 
 
-con.connect(function (err) {
+connection.connect(function (err) {
     if (err) return console.log("failed to connect to mysql server/ database", err);
     else return console.log("connection establish with Datebase!!!!");
 });
 
-const refreshAll = async () => {
-
-    await connection.query("DROP TABLE IF EXISTS test;", 
-    (err, result) => {
-        if (err) {
-            console.log(err)
-        }
-    });
-
-    await connection.query("CREATE TABLE test (id INT AUTO_INCREMENT NOT NULL, title varchar(255) NOT NULL, comment TEXT(65535), author varchar(255) NOT NULL, date TIMESTAMP DEFAULT CURRENT_TIMESTAMP, isOpen TINYINT(1) DEFAULT 1, primary key (id));", 
-    (err, result) => {
-        if (err) {
-            console.log(err)
-        }
-    });
-
-    await data.forEach(element => {
-        connection.query("INSERT INTO test (title, comment, author, isOpen) VALUES (?,?,?,?);",[element.title, element.comment, element.author, element.isOpen], (err, result) => {
-            if (err) {
-                console.log(err)
-            }
-        }) 
-    });
-}
-refreshAll()
-
-module.exports = con;
+module.exports = connection;
